dc7ffc97d4e7932c45ec75ac40fe65cac01b4b99
Jacob Chaffin <jchaffin@ucla.edu>
2025-11-14 13:36:30 -0800

    build to js

--- app.plugin.js
+++ app.plugin.js
@@ -1,1 +1,74 @@
-module.exports = require('./plugin/build');
+const {
+  AndroidConfig,
+  withGradleProperties,
+  withProjectBuildGradle,
+  withSettingsGradle,
+  withStringsXml,
+} = require('@expo/config-plugins');
+
+const withUnity = (config, { name = 'react-native-unity' } = {}) => {
+  config.name = name;
+  config = withProjectBuildGradleMod(config);
+  config = withSettingsGradleMod(config);
+  config = withGradlePropertiesMod(config);
+  config = withStringsXMLMod(config);
+  return config;
+};
+
+const REPOSITORIES_END_LINE = `maven { url 'https://www.jitpack.io' }`;
+
+const withProjectBuildGradleMod = (config) =>
+  withProjectBuildGradle(config, (modConfig) => {
+    if (modConfig.modResults.contents.includes(REPOSITORIES_END_LINE)) {
+      // use the last known line in expo's build.gradle file to append the newline after
+      modConfig.modResults.contents = modConfig.modResults.contents.replace(
+        REPOSITORIES_END_LINE,
+        REPOSITORIES_END_LINE +
+          '\nflatDir { dirs "${project(\':unityLibrary\').projectDir}/libs" }\n'
+      );
+    } else {
+      throw new Error(
+        'Failed to find the end of repositories in the android/build.gradle file`'
+      );
+    }
+    return modConfig;
+  });
+
+const withSettingsGradleMod = (config) =>
+  withSettingsGradle(config, (modConfig) => {
+    modConfig.modResults.contents += `
+include ':unityLibrary'
+project(':unityLibrary').projectDir=new File('../unity/builds/android/unityLibrary')
+    `;
+    return modConfig;
+  });
+
+const withGradlePropertiesMod = (config) =>
+  withGradleProperties(config, (modConfig) => {
+    modConfig.modResults.push({
+      type: 'property',
+      key: 'unityStreamingAssets',
+      value: '.unity3d',
+    });
+    return modConfig;
+  });
+
+// add string
+const withStringsXMLMod = (config) =>
+  withStringsXml(config, (config) => {
+    config.modResults = AndroidConfig.Strings.setStringItem(
+      [
+        {
+          _: 'Game View',
+          $: {
+            name: 'game_view_content_description',
+          },
+        },
+      ],
+      config.modResults
+    );
+    return config;
+  });
+
+module.exports = withUnity;
+  
